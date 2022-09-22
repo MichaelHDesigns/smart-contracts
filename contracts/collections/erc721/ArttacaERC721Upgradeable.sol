409a141501610f6cb227fe748c8e3e79ef499dee
Leon Acosta @ Dandelion Labs <leon@dandelionlabs.io>
2022-09-22 17:19:34 +0700

    Base implemention for IArrtacaERC721Upgradeable.sol

--- contracts/collections/erc721/ArttacaERC721Upgradeable.sol
+++ contracts/collections/erc721/ArttacaERC721Upgradeable.sol
@@ -4,17 +4,19 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+
+import "./IArrtacaERC721Upgradeable.sol";
 
 /**
  * @title ArttacaERC721Upgradeable
  * @dev This contract is an Arttaca ERC721 upgradeable collection.
  */
-contract ArttacaERC721Upgradeable is OwnableUpgradeable {
+contract ArttacaERC721Upgradeable is OwnableUpgradeable, ERC721Upgradeable, IArrtacaERC721Upgradeable {
 
-    string public name;
-    string public symbol;
     address[] public splits;
     uint[] public shares;
+    mapping(uint => bytes) mintDataList;
 
     function __ArttacaERC721_initialize(
         address _owner,
@@ -23,13 +25,20 @@ contract ArttacaERC721Upgradeable is OwnableUpgradeable {
         address[] memory _splits,
         uint[] memory _shares
     ) public initializer {
+        __ERC721_init(_name, _symbol);
         __Ownable_init();
         _transferOwnership(_owner);
 
-        name = _name;
-        symbol = _symbol;
         splits = _splits;
         shares = _shares;
     }
 
+    function mintAndTransfer(address _to, uint _tokenId) override external onlyOwner {
+        _mint(_to, _tokenId);
+    }
+
+    function mintAndTransfer(address _to, uint _tokenId, bytes calldata _mintData) override external onlyOwner {
+        mintDataList[_tokenId] = _mintData;
+        _mint(_to, _tokenId);
+    }
 }
---
+++ contracts/collections/erc721/IArrtacaERC721Upgradeable.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+// Arttaca Contracts (last updated v1.0.0) (collections/common/IArrtacaERC721Upgradeable.sol)
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
+
+/**
+ * @title Arttaca ERC721 interface, standard for Arttaca NFT collections.
+ *
+ * Contains the basic methods and functionalities that will be used for
+ * Arttaca collections.
+ */
+interface IArrtacaERC721Upgradeable is IERC721Upgradeable {
+
+    /**
+     * @dev Allows Owner to mint new assets in the collection.
+     *
+     * Requirements:
+     *
+     * - The `msg.sender` is the owner.
+     * - The value '_to' must be different than a ZERO address.
+     *
+     * Emits a {Transfer} event for every new asset minted.
+     */
+    function mintAndTransfer(address _to, uint _tokenId) external;
+
+    /**
+     * @dev Allows anyone to mint assets if there's a valid owner signature.
+     *
+     * Requirements:
+     *
+     * - The `msg.sender` is the owner.
+     * - The value '_to' must be different than a ZERO address.
+     * - The value '_mintData' must contain valid signature and token information.
+     *
+     * Emits a {Transfer} event for every new asset minted.
+     */
+    function mintAndTransfer(address _to, uint _tokenId, bytes calldata _mintData) external;
+}
