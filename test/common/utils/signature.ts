caecd3aa7b7f5c43ed1424492a6165f35dc85596
Leon Acosta @ Dandelion Labs <leon@dandelionlabs.io>
2022-11-14 17:37:25 +0700

    Added tests for buying in the marketplace

--- test/common/utils/signature.ts
+++ test/common/utils/signature.ts
@@ -16,9 +16,24 @@ async function createMintSignature(
     return await signHash(signer, hash);
 }
 
+async function createSaleSignature(
+    contractAddress: string,
+    signer: SignerWithAddress,
+    tokenId: BigNumber,
+    price: string,
+    expirationTimestamp: number
+): Promise<string> {
+    const hash = ethers.utils.solidityKeccak256(
+        ["address", "uint256", "uint256", "uint256"],
+        [contractAddress, tokenId, price, expirationTimestamp]
+    );
+    return await signHash(signer, hash);
+}
+
+
 
 function signHash(signer: SignerWithAddress, hash: string): Promise<string> {
     return signer.signMessage(ethers.utils.arrayify(hash));
 }
 
-export { createMintSignature }
+export { createMintSignature, createSaleSignature }
